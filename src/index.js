a5673d9f9d00f01995fd9db29b2a10fe3bf80d16
benardop <benard.oc.p@gmail.com>
2023-11-08 18:57:30 -0300

    Initail project files commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function CreditCard({cardInfo}) {
+  return (
+    <div className='credit-card'>
+       <Name text={cardInfo.holderName} />
+       <CardNumber  text={cardInfo.cardNumber}/>
+       <Bank text={cardInfo.bank}/>
+    </div>
+  )
+}
+
+const Name = ({text}) => {
+  return (
+    <div className='holder-name'>
+        {text}
+    </div>
+  )
+}
+
+const CardNumber = ({text}) => {
+  return (
+    <div className='card-number'>
+        {text}
+    </div>
+  )
+}
+
+const Bank = ({text}) => {
+  return (
+    <div className='bank-name'>
+      {text}
+    </div>
+  )
+}
+
+const testCardInfo = {
+  holderName: 'CardHolder Name',
+  cardNumber: '123 345 678 879',
+  civ: '234',
+  validy: '08/26',
+  bank: 'Big Bank Inc.'
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CreditCard cardInfo={testCardInfo} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
